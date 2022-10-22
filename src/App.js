f6a5c297cbcf420ee0921d1312a1b9a238cdf65b
Mohamed hedi Nasr <mednasr@users.noreply.github.com>
2022-10-23 01:38:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Main from "./pages/Main";
+import Favorites from "./components/Favorites";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="wrapper">
+            <Header className="App-header"/>
+            <Routes>
+                <Route exact path="/" element={<Main/>} />
+                <Route exact path="/favorites" element={<Favorites/>} />
+            </Routes>
+
+        </div>
+      </div>
   );
 }
 
