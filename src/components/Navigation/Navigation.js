a61ff5da23ab1c76ca9de4a32fbdbb30910a9631
Mohamed hedi Nasr <mednasr@users.noreply.github.com>
2022-10-23 02:03:39 +0200

    Create Navigation component + style

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import './Navigation.scss'
+//Navigation component to manage each joke related actions
+
+export default function NavigationComponent({ handleGetJoke, handleNewJoke }) {
+
+
+    let navigate = useNavigate();
+
+    const routeChange = () =>{
+        let path = `favorites`;
+        navigate(path);
+    }
+
+
+
+    return (
+        <>
+            <div className="main__content">
+                <form  onSubmit={handleNewJoke}>
+                    <button type="submit">Get jokes each 5 sec</button>
+                </form>
+                <button type="submit" onClick={routeChange}>Favorites</button>
+            </div>
+        </>
+    );
+}
