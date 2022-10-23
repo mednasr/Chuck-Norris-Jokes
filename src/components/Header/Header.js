b990f3f821bd530c51953c2eb3053dd45bf0282c
Mohamed hedi Nasr <mednasr@users.noreply.github.com>
2022-10-23 02:02:39 +0200

    Create Header component + style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.scss'
+
+export default function Header({ isFavoritesShow, setIsFavoritesShow }) {
+//header component ( header text can be extended )
+
+    return (
+        <header>
+            <h1>FrontValue 2022</h1>
+        </header>
+    );
+}
